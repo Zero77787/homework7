ddb1cf35af89ae521ed15bf2d5eb7d40decccfa1
zero77787 <rsokolov995@gmail.com>
2022-12-13 15:28:09 +0300

    Домашняя работа №8 (Новое)

---
+++ src/homework8/Main.java
@@ -0,0 +1,13 @@
+package homework8;
+
+public class Main {
+        public static void main(String[] args) {
+            Author author1 = new Author("Джоан","Роулинг" );
+            Author author2 = new Author("Андрей","Кочергин" );
+            Book book1 =  new Book("Гарри Поттер",1992,author1);
+            Book book2 =  new Book("Мужик с топором",2008,author2);
+            book1.setPublicationYear(2022);
+            System.out.println("book1 = " + book1);
+        }
+    }
+
